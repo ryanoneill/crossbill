316f3311190f15bb10134c28116c1b49eb24781c
Ryan O'Neill <ryanoneill@gmail.com>
2021-02-11 22:40:01 -0800

    toString cleanup

--- src/main/scala/com/twitter/crossbill/ResultType.scala
+++ src/main/scala/com/twitter/crossbill/ResultType.scala
@@ -1,21 +1,15 @@
 package com.twitter.crossbill
 
-case class ResultType(values: Seq[ValueType])
+case class ResultType(values: Seq[ValueType]) {
+  override def toString: String = 
+    if (values == Seq.empty) "nil"
+    else values.mkString("(", ",", ")")
+}
 
 object ResultType {
-  val Nil = new ResultType(Seq.empty) {
-    override def toString: String = "ResultType.Nil"
-  }
-  val I32 = new ResultType(Seq(ValueType.I32)) {
-    override def toString: String = "ResultType.I32"
-  }
-  val I64 = new ResultType(Seq(ValueType.I64)) {
-    override def toString: String = "ResultType.I64"
-  }
-  val F32 = new ResultType(Seq(ValueType.F32)) {
-    override def toString: String = "ResultType.F32"
-  }
-  val F64 = new ResultType(Seq(ValueType.F64)) {
-    override def toString: String = "ResultType.F64"
-  }
+  val Nil = new ResultType(Seq.empty)
+  val I32 = new ResultType(Seq(ValueType.I32))
+  val I64 = new ResultType(Seq(ValueType.I64))
+  val F32 = new ResultType(Seq(ValueType.F32))
+  val F64 = new ResultType(Seq(ValueType.F64))
 }
--- src/main/scala/com/twitter/crossbill/ValueType.scala
+++ src/main/scala/com/twitter/crossbill/ValueType.scala
@@ -3,8 +3,16 @@ package com.twitter.crossbill
 sealed trait ValueType
 
 object ValueType {
-  case object I32 extends ValueType
-  case object I64 extends ValueType
-  case object F32 extends ValueType
-  case object F64 extends ValueType
+  case object I32 extends ValueType {
+    override def toString: String = "i32"
+  }
+  case object I64 extends ValueType {
+    override def toString: String = "i64"
+  }
+  case object F32 extends ValueType {
+    override def toString: String = "f32"
+  }
+  case object F64 extends ValueType {
+    override def toString: String = "f64"
+  }
 }
